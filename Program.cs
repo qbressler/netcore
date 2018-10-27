23962f596d22531a459265df67713ff8e5f52c79
quintin <=>
2018-10-27 16:28:44 -0400

    first commit

---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace c_
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+					int x,y;
+					x = 10;
+					y = 20;
+					Console.WriteLine(x*y);
+          Console.WriteLine("Hello World!");
+        }
+    }
+}
